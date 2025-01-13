4226d6ec8d1b2712876ae29dbc845135538ad7a7
Sanskruti <sanskrutinerlekar28800@gmail.com>
2025-01-13 07:10:47 +0530

    setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(file_path:str)-> List[str]:
         requirements = [req.replace("\n"," ") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+            requirements.remove(HYPEN_E_DOT )
 
 setup(
     name = 'ML_Project',
